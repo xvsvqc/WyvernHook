71368d3c56822711b30b713264a4484187dc097b
xvsvqc <104143318+xvsvqc@users.noreply.github.com>
2022-08-07 13:17:55 +0100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -2180,7 +2180,7 @@ do
 
     Library.Watermark = WatermarkOuter;
     Library.WatermarkText = WatermarkLabel;
-    Library:MakeDraggable(Library.Watermark);
+    --Library:MakeDraggable(Library.Watermark);
 
 
 
